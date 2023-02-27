799b6040a16af63ec0836fcdb8111204d473f260
Basit Manzoor <49262108+basitmanzoor@users.noreply.github.com>
2023-02-27 14:42:43 +0530

    Time: 3 ms (68.95%), Space: 44.1 MB (28.93%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,25 +1,72 @@
 class Solution {
     public int longestOnes(int[] arr, int k) {
         int n = arr.length;
-        int numOfZeroesInCurrWindow = 0;
-		int sp = 0;
-		int ep = 0;
-		int ans = 0;
+        int noOfZeros = 0;
+        int sp = 0;
+        int ep = 0;
+        
+        int ans = 0;
+        
+        while(ep < n){
+            if(arr[ep] == 0){
+                noOfZeros++;
+            }
+            
+            while(noOfZeros > k){
+                if(arr[sp] == 0){
+                    noOfZeros--;
+                }
+                sp++;
+            }
+            ans = Math.max(ans, ep-sp+1);
+            ep++;
+        }
+        return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         int n = arr.length;
+//         int numOfZeroesInCurrWindow = 0;
+// 		int sp = 0;
+// 		int ep = 0;
+// 		int ans = 0;
 
-		while(ep<n){
-			//end point is visited now
-			if(arr[ep] == 0) numOfZeroesInCurrWindow++;
+// 		while(ep<n){
+// 			//end point is visited now
+// 			if(arr[ep] == 0) numOfZeroesInCurrWindow++;
 
-			// -------------------
-			// shrink until window is valid
-			while(numOfZeroesInCurrWindow > k){
-				if(arr[sp] == 0) numOfZeroesInCurrWindow--;
-				sp++;
-			}
-			//on this line of code, our window will always be valid
-			ans = Math.max(ans,ep-sp+1);
-			ep++;
-		}
-		return ans;
+// 			// -------------------
+// 			// shrink until window is valid
+// 			while(numOfZeroesInCurrWindow > k){
+// 				if(arr[sp] == 0) numOfZeroesInCurrWindow--;
+// 				sp++;
+// 			}
+// 			//on this line of code, our window will always be valid
+// 			ans = Math.max(ans,ep-sp+1);
+// 			ep++;
+// 		}
+// 		return ans;
     }
 }
